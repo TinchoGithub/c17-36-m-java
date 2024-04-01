2f66d79fc07c5098c3437d9db8bb3f790c31b558
TinchoGithub <perezjmartin12@gmail.com>
2024-04-01 00:15:27 -0300

    create Role entity and the repository

--- backend/user-service/src/main/java/com/c1736/userservice/entities/Role.java
+++ backend/user-service/src/main/java/com/c1736/userservice/entities/Role.java
@@ -1,2 +1,37 @@
-package com.c1736.userservice.entities;public class Role {
+package com.c1736.userservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- backend/user-service/src/main/java/com/c1736/userservice/repository/IRoleRepository.java
+++ backend/user-service/src/main/java/com/c1736/userservice/repository/IRoleRepository.java
@@ -1,2 +1,9 @@
-package com.c1736.userservice.repository;public interface IRoleRepository {
+package com.c1736.userservice.repository;
+
+import com.c1736.userservice.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRoleRepository extends JpaRepository<Role, Long> {
 }
