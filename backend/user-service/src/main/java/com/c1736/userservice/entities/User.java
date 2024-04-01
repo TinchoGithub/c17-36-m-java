3664cead8dcd450071b26f46c3724126dd4c68d3
TinchoGithub <perezjmartin12@gmail.com>
2024-04-01 19:14:02 -0300

    add properties to user phone field and gradlew.bat to gitignore

--- backend/user-service/src/main/java/com/c1736/userservice/entities/User.java
+++ backend/user-service/src/main/java/com/c1736/userservice/entities/User.java
@@ -1,6 +1,8 @@
 package com.c1736.userservice.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.io.Serializable;
@@ -25,7 +27,9 @@ public class User implements Serializable {
     @Column(unique = true)
     private String password;
     @Column(length = 15)
-    private String tel;
+    @Pattern(regexp = "^\+?54\s9\d{2}\s\d{3}\s\d{4}$", message = "El campo 'numberPhone' debe ser un número de teléfono celular válido. Ingrese el formato +54 9XX XXXX XXXX")
+    @NotBlank(message = "El campo 'phone' es obligatorio")
+    private String phone;
     @ManyToOne
     @JoinColumn(name = "id_role")
     private Role role;
---
+++ backend/user-service/src/main/java/com/c1736/userservice/service/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.c1736.userservice.service.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException() {
+        super();
+    }
+}
