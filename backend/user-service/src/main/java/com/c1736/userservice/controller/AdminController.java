0fb7cca4e36f3185d3a0655c4b0c08db2a63193b
pegaso02 <fabianmanrique10@gmail.com>
2024-04-03 18:56:43 -0500

    Resolved merge conflicts after stash pop

--- backend/user-service/src/main/java/com/c1736/userservice/controller/AdminController.java
+++ backend/user-service/src/main/java/com/c1736/userservice/controller/AdminController.java
@@ -1,9 +1,29 @@
 package com.c1736.userservice.controller;
 
+import com.c1736.userservice.entities.User;
+import com.c1736.userservice.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin")
 public class AdminController {
+
+    @Autowired
+    private IUserService userService;
+
+    // @PreAuthorize
+    @GetMapping("/findUser{id}")
+    public User findUserById(Long id){
+        return userService.findUserById(id);
+    }
+
+    public List<User>findAllUsers(){
+        return userService.findAllUsers();
+    }
+
 }
