d8e339d52cd95406b58a825e8a707547ae994df5
samuel <saquino@nerdcom.pro>
2024-04-04 19:44:39 -0400

    redux-toolkit-added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
