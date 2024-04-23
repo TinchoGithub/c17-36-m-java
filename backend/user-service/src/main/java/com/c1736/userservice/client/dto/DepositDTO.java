47aff6505732d3a827ad64b4c4bdb0b51349713c
Martín Perez <100448340+TinchoGithub@users.noreply.github.com>
2024-04-23 15:31:28 -0300

    rename branch

--- backend/user-service/src/main/java/com/c1736/userservice/client/dto/DepositDTO.java
+++ backend/user-service/src/main/java/com/c1736/userservice/client/dto/DepositDTO.java
@@ -1,2 +1,18 @@
-package com.c1736.userservice.client.dto;public class DepositDTO {
+package com.c1736.userservice.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class DepositDTO {
+
+    private Long idAccount;
+    private BigDecimal amount;
+    private LocalDateTime transactionDate;
 }
--- backend/user-service/src/main/java/com/c1736/userservice/client/dto/TransferDTO.java
+++ backend/user-service/src/main/java/com/c1736/userservice/client/dto/TransferDTO.java
@@ -1,8 +1,15 @@
 package com.c1736.userservice.client.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class TransferDTO {
 
     private BankDTO fromAccount;
